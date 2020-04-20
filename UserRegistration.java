3dc798424b40a1e4c8fd34a8e84420d3d48bf28b
250399 <kunaldeshmukh2503@gmail.com>
2020-04-19 22:44:19 -0700

    ADDED: Email should be in valid format

--- UserRegistration.java
+++ UserRegistration.java
@@ -7,5 +7,7 @@ public class UserRegistration {
     public boolean validateName(String firstNamePattern, String firstName){
         return Pattern.matches(firstNamePattern,firstName);
     }
-
+    public boolean validateEmail(String EmailPattern, String EmailId){
+        return Pattern.matches(EmailPattern,EmailId);
+    }
 }
--- UserRegistrationTest.java
+++ UserRegistrationTest.java
@@ -9,4 +9,10 @@ public class UserRegistrationTest {
     public void checkName(){
         Assert.assertTrue(object.validateName("^[A-Z]{1}[a-z]{2,}[ ][A-Z]{1}[a-z]{2,}$","Kunal Deshmukh"));
     }
+    @Test
+    public void Email(){
+        Assert.assertTrue(object.validateName("^[a-zA-Z0-9]+[\\.\\-\\+\\_]?[a-zA-Z0-9]*@[a-zA-Z]+[.]?[a-zA-Z]{2,4}[\\.]?([a-z]{2})?$","Kunal.Deshmukh@gmail.com.in"));
+    }
+
+
 }
